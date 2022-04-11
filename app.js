13f29bb0ba8c40c973c831880b671ea6f72d4203
am-gfource <aliciagarcia61@gmail.com>
2022-04-10 19:04:13 -0500

    fixed navbar and logo

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
-})
 
-var myDropdown = document.getElementsByClassName('.dropdown-toggle')
-myDropdown.addEventListener('show.dropdown', function () {
-
-})
 
 
 
